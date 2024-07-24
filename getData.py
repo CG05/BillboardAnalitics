7a54cf4b03af3cfe1eb9567b5e19cda33c64a637
CG05 <sungcjfrb2@gmail.com>
2024-07-24 18:01:08 +0900

    .

---
+++ getData.py
@@ -0,0 +1,9 @@
+import getBillboardData
+import getSpotifyData
+
+def getData(s_year, e_year):
+    getBillboardData.getBillboardData(s_year, e_year)
+    getSpotifyData.getSpotifyData(s_year, e_year)
+    
+# 시작년, 끝년 넣고 실행
+getData(2010, 2020)
