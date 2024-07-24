0d393631341b5b67e1a7d64abd1ed818c037f7ce
CG05 <81802328+CG05@users.noreply.github.com>
2024-07-24 21:37:41 +0900

    ,

--- getData.py
+++
@@ -1,9 +0,0 @@
-import getBillboardData
-import getSpotifyData
-
-def getData(s_year, e_year):
-    getBillboardData.getBillboardData(s_year, e_year)
-    getSpotifyData.getSpotifyData(s_year, e_year)
-    
-# 시작년, 끝년 넣고 실행
-getData(2007, 2022)
