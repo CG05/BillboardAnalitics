a748614fa2975e11464b3a082047ae6b284087e3
hcm1206 <hcm1206@naver.com>
2024-07-30 14:34:39 +0900

    빌보드 차트 페이지 베이스 구현

--- server/billboardchart/urls.py
+++ server/billboardchart/urls.py
@@ -1,8 +1,11 @@
 from .views import billboardchart
-from django.urls import path, include
+from django.urls import path, re_path
+from django.views.generic import RedirectView
+from datetime import datetime
 
 app_name = "billboardchart"
 
 urlpatterns = [
-    path("", billboardchart)
+    path("", RedirectView.as_view(url='/billboardchart/' + datetime.now().strftime('%Y-%m-%d'), permanent=True)),
+    re_path(r'^(\d{4}-\d{2}-\d{2})$', billboardchart),
 ]
