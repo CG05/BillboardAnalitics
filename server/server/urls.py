47c80666775f809e862dacd2c27cd8797a7e764d
hcm1206 <hcm1206@naver.com>
2024-07-31 14:16:12 +0900

    장고 탬플릿 구조 대혁명

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.mainpage),
+    path("example", views.examplepage),
     path("introduction/", include('introduction.urls')),
     path("simulation/", include('simulation.urls')),
     path("history/", include('history.urls')),
--- server/server/views.py
+++ server/server/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def mainpage(request):
     return render(request, 'main.html')
+
+def examplepage(request):
+    return render(request, 'contentexample.html')
